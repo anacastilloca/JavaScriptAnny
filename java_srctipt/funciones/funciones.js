536f068466d45d454ecdf9be584f5f5b77cdb29d
Ana Castillo <ana.castillo@epn.edu.ec>
2017-05-03 14:40:57 -0500

    funciones

---
+++ java_srctipt/funciones/funciones.js
@@ -0,0 +1,20 @@
+/**
+ * Created by USRDEL on 5/3/17.
+ */
+
+function holaMundo() {
+    console.log('Holaa');
+}
+holaMundo();
+
+// El doble de un entero
+function doble(a) {
+    return a*2;
+}
+console.log(doble(5));
+
+//Multiplicacion
+function multiplicacion(a,b) {
+    return a*b;
+}
+console.log(multiplicacion(9,4));
