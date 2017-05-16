9cc4a2ff9653ea6aca652200564a0cb9ec4803dd
Ana Castillo <ana.castillo@epn.edu.ec>
2017-05-16 15:35:09 -0500

    TYPE SCRIPT arrays, number, string, concatenation

--- type_script/hola.ts
+++ type_script/hola.ts
@@ -1,5 +1,18 @@
 /**
  * Created by USRDEL on 5/16/17.
  */
-let hola= `Como vas`;
-console.log("HOLAAA");
+/*let hola= `Como vas`;
+console.log("HOLAAA");*/
+
+//let soltero: boolean="aaa"; ya no se puede igualar a lo que se quiera
+let soltero: boolean=true;//false
+//number
+let num=2;
+//string
+let frase="Holaaaa otra vez";
+//Concatenar
+let otrafrease= `Cuntas veces has dicho hola: ${num+3}`;
+//Arrays
+let notas:number[]=[1,2,3,4,5]; //[]
+let reprobada:Array<number>=[1,2,3,4,5,6]; //[]
+
