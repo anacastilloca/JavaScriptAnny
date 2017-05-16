61d9a31be6de08548b75362fe1065d475a0f8b69
Ana Castillo <ana.castillo@epn.edu.ec>
2017-05-16 15:23:52 -0500

    TYPE SCRIPT OJOOOOOO

---
+++ type_script/hola.js
@@ -0,0 +1,5 @@
+/**
+ * Created by USRDEL on 5/16/17.
+ */
+var hola = "Como vas";
+console.log("HOLAAA");
---
+++ type_script/hola.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by USRDEL on 5/16/17.
+ */
+let hola= `Como vas`;
+console.log("HOLAAA");
