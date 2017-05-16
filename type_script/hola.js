b5b15adafa09357e859198f1ace7cf0a22edace8
Ana Castillo <ana.castillo@epn.edu.ec>
2017-05-16 15:48:59 -0500

    TYPE SCRIPT interfaces

--- type_script/hola.js
+++ type_script/hola.js
@@ -1,5 +1,26 @@
 /**
  * Created by USRDEL on 5/16/17.
  */
-var hola = "Como vas";
-console.log("HOLAAA");
+/*let hola= `Como vas`;
+console.log("HOLAAA");*/
+//let soltero: boolean="aaa"; ya no se puede igualar a lo que se quiera
+var soltero = true; //false
+//number
+var num = 2;
+//string
+var frase = "Holaaaa otra vez";
+//Concatenar
+var otrafrease = "Cuntas veces has dicho hola: " + (num + 3);
+//Arrays
+var notas = [1, 2, 3, 4, 5]; //[]
+var reprobada = [1, 2, 3, 4, 5, 6]; //[]
+//Con any se puede simular  los tipo de datos de java script
+//con eso podemos igualar nombre=12 sin error..
+var nombre = "aa";
+nombre = 12;
+//Objeto
+var usuario = {
+    nombre: "Anny",
+    apellido: "Castillo",
+    direccion: "sss2" //Este es un parametro opcional puede estar como no
+};
--- type_script/hola.ts
+++ type_script/hola.ts
@@ -13,6 +13,23 @@ let frase="Holaaaa otra vez";
 //Concatenar
 let otrafrease= `Cuntas veces has dicho hola: ${num+3}`;
 //Arrays
-let notas:number[]=[1,2,3,4,5]; //[]
-let reprobada:Array<number>=[1,2,3,4,5,6]; //[]
+let notas: number[]=[1,2,3,4,5] //[]
+let reprobada:Array<number>=[1,2,3,4,5,6]//[]
+//Con any se puede simular  los tipo de datos de java script
+//con eso podemos igualar nombre=12 sin error..
+let nombre:any="aa";
+nombre=12;
+
+//Objeto
+let usuario:UsuarioLogin={
+    nombre:"Anny",
+    apellido:"Castillo",
+    direccion:"sss2" //Este es un parametro opcional puede estar como no
+}
+interface UsuarioLogin{
+    nombre:string,
+    apellido:string,
+    //para que sea opcional se pone ?
+    direccion?:string
+}
 
