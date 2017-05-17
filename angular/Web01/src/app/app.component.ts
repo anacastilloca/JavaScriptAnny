11de888c903867e4520d67646291ee982566893d
Ana Castillo <ana.castillo@epn.edu.ec>
2017-05-17 14:37:34 -0500

    ANGULAR modificaciones

--- angular/Web01/src/app/app.component.ts
+++ angular/Web01/src/app/app.component.ts
@@ -2,10 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'js-raiz',
-  templateUrl: './app.component.html',
+  
+  template: "<h1>Hola</h1>",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app primera!';
-  nombre='mmmmmmmm';
+  usuario:UsuarioInterface= {
+    nombre:"Ana",
+    apellido:"Castillo"
+  }
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    console.log(this.saludar(this.usuario.nombre,this.usuario.apellido))
+  }
+  holaMundo(){
+    console.log("Hola mundosss");
+  }
+  saludar(nombre:string,apellido?:string){
+    return `Hola ${nombre} ${apellido}`;
+  }
+
+}
+interface UsuarioInterface{
+  nombre:string,
+  apellido:string
 }
