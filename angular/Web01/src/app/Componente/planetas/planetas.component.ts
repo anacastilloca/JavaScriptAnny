22683de0fc995d767fbf0742fdbcf367ac2f4cde
Ana Castillo <ana.castillo@epn.edu.ec>
2017-06-06 16:35:28 -0500

    aggregation por cambiar de planetas
    funciona cargar Planetas

--- angular/Web01/src/app/Componente/planetas/planetas.component.ts
+++ angular/Web01/src/app/Componente/planetas/planetas.component.ts
@@ -8,28 +8,9 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./planetas.component.css']
 })
 export class PlanetasComponent implements OnInit {
-  nombre: string = "Ana";
 
   //planetas=[];
   planetas : PlanetaStarWars[] =[];
-
-  arregloUsuarios = [{
-    nombre: "Ana",
-    apellido: "Castillo",
-    conectado: true
-  }, {
-    nombre: "Pedro",
-    apellido: "Maldonado",
-    conectado: true
-  }, {
-    nombre: "Jhon",
-    apellido: "Rodriguez",
-    conectado: false
-  }, {
-    nombre: "jUAN",
-    apellido: "Maldonado",
-    conectado: true
-  }];
 
   constructor(private _http:Http) {
     //Inicia la clase
@@ -41,20 +22,6 @@ export class PlanetasComponent implements OnInit {
     // esta listo
   }
 
-  cambiarNombre(): void {
-    this.nombre = "Andreaa"
-  }
-
-  cambiarOtroNombre() {
-    this.nombre = "Mariaa"
-  }
-
-  cambiarNombreInput(nombreEtiqueta) {
-    console.log(nombreEtiqueta.value);
-    console.log(nombreEtiqueta.type);
-    console.log(nombreEtiqueta.placeholder);
-    this.nombre = nombreEtiqueta.value
-  }
   cargarPlanetas(){
     this._http.get("http://swapi.co/api/planets")
     //.map(response=>response.json())
@@ -66,6 +33,13 @@ export class PlanetasComponent implements OnInit {
           let respuesta=response.json();
           console.log("Holaa",respuesta.next);
           this.planetas=respuesta.results;
+
+          this.planetas=this.planetas.map(
+            (planeta)=>{
+              planeta.imagenUrl="assets/imagenes"+planeta.name+'.jpg';
+              return planeta;
+            }
+          )
         },
         (error)=>{
           console.log("Error: ",error);
@@ -92,4 +66,5 @@ interface PlanetaStarWars{
   created: Date;
   edited: Date;
   url: string;
+  imagenUrl?:string;
 }
