db5ad9da4dec08d8c9894387e62cd57f91afcbd9
annyCris <ana.castillo@epn.edu.ec>
2017-06-11 17:48:35 -0500

    Creacion de componentes
    Componenetes para planeta de star wars

---
+++ angular/Web01/src/app/Componente/planeta-star-wars/Componentes PSW/orbital-period/orbital-period.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orbital-period',
+  templateUrl: './orbital-period.component.html',
+  styleUrls: ['./orbital-period.component.css']
+})
+export class OrbitalPeriodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
